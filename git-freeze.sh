978aeb209e2a93ca89dcafe29a30f64d7c0e069c
Peter McCracken <peter.mccracken@jadedpixel.com>
2013-11-08 23:52:38 -0500

    better output squashing

--- git-freeze.sh
+++ git-freeze.sh
@@ -1,3 +1,3 @@
 function git-frozen () {
-  git rev-list HEAD -n1 --pretty=oneline | grep "^WIP \[(?:UN)?STAGED\]$" > /dev/null
+  git rev-list HEAD -n1 --pretty=oneline | grep "^WIP \[(?:UN)?STAGED\]$" 2>/dev/null
 }
