555608ceb2b59dd2ba7844770ca2c468f9f0a3b3
Peter McCracken <peter@petermccracken.com>
2022-08-02 16:54:39 -0400

    change format of WIP message

--- git-freeze.sh
+++ git-freeze.sh
@@ -1,4 +1,4 @@
 function git-frozen () {
-  git rev-list HEAD -n1 --pretty=oneline | grep "WIP \[\(UN\)\?STAGED\]$" &>/dev/null
+  git rev-list HEAD -n1 --pretty=oneline | grep "\[WIP\] \(UN\)\?STAGED$" &>/dev/null
   return $?
 }
